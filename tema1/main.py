9d18de44fcb3c2a597630bb814f4cebf1b5bcce5
Andrés P <spelujo@gmail.com>
2025-10-24 14:26:23 +0200

    Initial commit

---
+++ tema1/main.py
@@ -0,0 +1,17 @@
+num = 4
+str1 = "rojo"
+str2 = str1
+verda = True
+
+print(num, str1, str2, verda)
+
+x = 5
+y = 7
+
+print((x + y), (y - x), (x * y), (y / x))
+
+# x = int(input("Escribe un número: "))
+# print(x)
+
+texto = "Este texto es muy bonito"
+print("Aquel" + texto[4:])
