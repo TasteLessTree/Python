3d59f9e795f7bef67c219117bbb31130b6c810ac
Andrés P <spelujo@gmail.com>
2025-11-21 14:21:32 +0100

    Ejercicio 2

--- ejercicios/Ejercicio2.py
+++ ejercicios/Ejercicio2.py
@@ -1,5 +1,6 @@
 import datetime
 import locale
+import random
 
 try:
     # Para Linux
@@ -13,9 +14,37 @@ except locale.Error:
 
 registro_invitados = {}
 
+print("--- REGISTRO DE INVITADOS A LA CENA DE NAVIDAD ---")
 i = 1
 while i <= 5:
-    nombre = input(f'Introdu el nombre del invitado: {i}/5: ')
-    ahora = datetime.datetime.now()
+    nombre = input(f'Introduce el nombre del invitado: {i}/5: ')
+    fecha = datetime.datetime.now()
+    dia = fecha.strftime("%d de %B")
+    hora = fecha.strftime("%H:%M:%S")
 
+    print(f'\t-> {nombre} ha sido añadido a las {hora}')
+
+    registro_invitados.update(
+        {
+            i: {
+                "nombre": nombre,
+                "dia": dia,
+                "hora": hora
+            }
+        }
+    )
     i += 1
+
+print("--- ¡Registro completado! ---")
+
+print("--- LISTA ORDENADA (Por registro) ---")
+lista_invitados_ordenada = []
+
+print("--- LISTA DESORDENADA (Sorteo de asientos) ---")
+lista_desordenada = lista_invitados_ordenada.copy()
+random.shuffle(lista_desordenada)
+
+print("--- ANÁLISIS DE LA LISTA ---")
+print(f'Total de invitados apuntados: {len(registro_invitados)}')
+
+datos = registro_invitados.values()
