673f01f765a4feb6bbf5399ee94aa6a48bb26fe9
TLT <spelujo@gmail.com>
2026-01-20 09:28:54 +0100

    Ejempolo de fastapi y sqlite

--- main.py
+++ main.py
@@ -1,13 +1,63 @@
-# Ejemplo con fastAPI
-
+# Ejemplo con fastAPI y una mini base de datos
+import sqlite3
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-@app.get("/calcular/{param1}/{param2}")
-async def calcular(param1: int, param2: int) -> int:
-    total: int = param1 + param2
-    return total
+class Usuario(BaseModel):
+    id: int
+    nombre: str
+    email: str
+    clave: str
+
+@app.get('/usuarios')
+async def get_usuarios() -> dict[str, list[Usuario]] | dict[str, str]:
+    try:
+        conn = sqlite3.connect('prueba.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM usuarios')
+        rows = cursor.fetchall()
+
+        usuarios = [
+            Usuario(
+                id = rows[0],
+                nombre = rows[1],
+                email = rows[2],
+                clave = rows[3],
+            )
+        ]
+
+        conn.close()
+        return {'usuarios': usuarios}
+    except sqlite3.OperationalError:
+        return {'error': 'Error al abrir la base de datos. No se pudieren obtener los usuarios.'}
+
+@app.get('/usuarios/{id}')
+async def get_usuario(id: int) -> dict[str, str]:
+    try:
+        conn = sqlite3.connect('prueba.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM usuarios WHERE id = ?', (id,))
+        row = cursor.fetchone()
+
+        usuario = Usuario(
+            id = row[0],
+            nombre = row[1],
+            email = row[2],
+            clave = row[3],
+        )
+
+        conn.close()
+        return {'usuario': usuario}
+    except sqlite3.OperationalError:
+        return {'error': f'Error al abrir la base de datos. No se pudo obtener el usuario con id: {id}.'}
+
+"""@app.post('/crear_usuario')
+async def crear_usuario(usuario: Usuario):
+    try:
+        conn = sqlite3.('prueba.db')
+        cursor = conn.cursor()
+
+        cursor.execute('''
+        ''')"""
